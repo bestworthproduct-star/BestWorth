0c56f9b6ea5f98b16323164ec14bb3a14d160afc
bestworthproduct-star <bestworthproduct@gmail.com>
2026-08-19 14:23:54 +0100

    Admin New UI

--- app/server/index.js
+++ app/server/index.js
@@ -134,7 +134,7 @@ mongoose.connection.on('error', (error) => {
 });
 
 if (isProduction && fs.existsSync(frontendDist)) {
-  app.get('*', (req, res) => {
+  app.get('(.*)', (req, res) => {
     res.sendFile(path.join(frontendDist, 'index.html'));
   });
 }
